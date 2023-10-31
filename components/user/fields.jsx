10f8138f1e6e8c1275889c89ef78917f809fcab3
Olamilekan.akinwa <olamilekan.akinwa@etranzactglobal.com>
2023-10-31 18:07:33 +0100

    user dashboard

--- components/user/fields.jsx
+++ components/user/fields.jsx
@@ -1,12 +1,17 @@
 // Fields.js
 import { useSelector } from 'react-redux';
 import { useGetallBooksQuery } from '@/app/apiSlices/book';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import BookComponent from './book-component';
 
 const Fields = () => {
   const { data, isLoading, isError } = useGetallBooksQuery();
-  const authToken = useSelector((state) => state.auth.token); // Retrieve the authToken
+  const authToken = useSelector((state) => state.auth.token); // Retrieve the authToken'
+  const [books, setBooks] = useState([])
+
+  useEffect(()=>{
+    setBooks(data)
+  }, [data])
 
   return (
     <div className='px-6'>
@@ -16,7 +21,7 @@ const Fields = () => {
       <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-x-4 gap-y-12">
         {data?.map((book) => (
           // Check if the book has a valid id before rendering
-          book.id ? (
+          book._id ? (
             <BookComponent
               key={book.id}
               id={book.id} // Pass the book's ID as a prop
@@ -34,7 +39,7 @@ const Fields = () => {
       <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-x-4 gap-y-12">
         {data?.map((book) => (
           // Check if the book has a valid id before rendering
-          book.id ? (
+          book._id ? (
             <BookComponent
               key={book.id}
               id={book.id} // Pass the book's ID as a prop
